d8ea8d1ef684225f2560e28e4c9c9eb622e6da03
Fabricio <fabricioroberto360@gmail.com>
2024-09-19 09:58:22 -0300

    pequenas alterações

--- pagina/informações.js
+++ pagina/informações.js
@@ -1,22 +1,31 @@
 const presentations = document.querySelectorAll('.presentation');
 
-// Função para alterar o conteúdo da div ao clicar
 function alterarConteudo(event) {
     const presentation = event.currentTarget;
 
-    // Substitui o conteúdo da div clicada com um link "Exit"
-    presentation.innerHTML = `
-        <img class="ft" src="/imgs/lixos.jpg" alt="Nova Imagem">
-        <div class="text">
-            <ul>
-                <li>Número: (79) 9xxxx-xxxx</li>
-                <li>Email: xxxxxx@gmail.com</li>
-                <li>Instagram: xxxxxx</li>
-            </ul>
-        </div>
-    `;
+    // Verifica se já tem o conteúdo alterado, para retornar ao original
+    if (presentation.dataset.originalContent) {
+        presentation.innerHTML = presentation.dataset.originalContent;  // Retorna ao conteúdo original
+        presentation.removeAttribute('data-original-content'); // Remove o atributo para restaurar o comportamento original
+    } else {
+        
+        presentation.dataset.originalContent = presentation.innerHTML;
+
+        
+        presentation.innerHTML = `
+            <img class="ft" src="/imgs/lixos.jpg" alt="Nova Imagem">
+            <div class="text">
+                <ul>
+                    <li>Número: (79) 9xxxx-xxxx</li>
+                    <li>Email: xxxxxx@gmail.com</li>
+                    <li>Instagram: xxxxxx</li>
+                </ul>
+            </div>
+        `;
+    }
 }
 
+// Adiciona o evento de clique para cada apresentação
 presentations.forEach(presentation => {
     presentation.addEventListener('click', alterarConteudo);
 });
